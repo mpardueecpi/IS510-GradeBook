ac763890568572d1afb27171a30428b9fbd76796
Juan Galvez <juagal4098@students.ecpi.edu>
2013-02-07 22:03:12 -0500

    Added Notes

---
+++ Users v1.2.java
@@ -0,0 +1,34 @@
+/**
+*  Class Users is the superclass for the Gradebook Project
+*
+*  @author Juan Galvez
+*  @version 1.2 02/08/2013
+*
+*/
+
+public class Users {
+
+  private String FirstName;
+  private String LastName;
+
+  
+
+  public void setLastname(string inLastName) {
+	this.LastName = inLastName;
+  }
+
+  public String getLastName() {
+	return this.LastName;
+  }
+
+  public void setFirstName(string inFirstName) {
+	this.FirstName = inFirstName;
+  }
+
+  public String getFirstName() {
+	return FirstName;
+  }
+
+  
+
+}
