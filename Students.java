e2999e79de5682f66beee7277abfcfe130964a57
Kasam <kasham4590@students.ecpi.edu>
2013-02-07 21:42:12 -0500

    Students File
    my student file
    
    Kasam

---
+++ Students.java
@@ -0,0 +1,33 @@
+import java.util.Vector;
+
+public class Students extends Users {
+
+  public String studentID;
+
+  public String major;
+
+    /**
+   * 
+   * @element-type Assignments
+   */
+  public Vector  myAssignments;
+
+  public void setStudentID() {
+  }
+
+  public String getStudentID() {
+  return null;
+  }
+
+  public void setMajor() {
+  }
+
+  public String getMajor() {
+  return null;
+  }
+
+  public String viewGrades() {
+  return null;
+  }
+
+}
