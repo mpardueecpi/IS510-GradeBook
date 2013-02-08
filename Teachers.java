ee635a8d6ab17cc0f15d6119ecf02d3ee483f5c7
mahaffeymg@me.com <mahaffey@server.example.private>
2013-02-07 21:39:29 -0500

    Teachers

---
+++ Teachers.java
@@ -0,0 +1,61 @@
+package edu.ecpi.is510;
+
+import Integer[];
+import String[];
+import String;
+import Integer;
+
+public class Teachers extends Users 
+{
+
+  public Integer teacherID;
+
+  public void setTeacherID(integer teacherIdentification) 
+  {
+  this.teacherID = teacherIdentification;
+  }
+
+  public Integer getTeacherID(teacherID) 
+  {
+  
+  return teacherID;
+  
+  }
+
+  public void createAssignment() 
+  {
+  
+  }
+
+  public void editAssignment() 
+  {
+  
+  }
+
+  public String[] listAssignments() 
+  {
+  
+  return null;
+  
+  }
+
+  public void enterGrades() 
+  {
+  
+  }
+
+  public Integer[] viewGrades() 
+  {
+  
+  return null;
+  
+  }
+
+  public String viewTeacherGradeCharts() 
+  {
+  
+  return null;
+  
+  }
+
+}
