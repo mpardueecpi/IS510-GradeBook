bd6414f5c8a7cbece2715177d4b8f817e75c1c59
galvezjc <juagal4098@students.ecpi.edu>
2013-02-07 18:42:26 -0800

    Create Users.java

---
+++ Users.java
@@ -0,0 +1,28 @@
+
+
+public class Users {
+
+  private String FirstName;
+  private String LastName;
+
+  
+
+  public void setLastname(string inLastName) {
+  this.LastName = inLastName;
+  }
+
+  public String getLastName() {
+	return this.LastName;
+  }
+
+  public void setFirstName(string inFirstName) {
+	this.FirstName = inFirstName;
+  }
+
+  public String getFirstName() {
+	return FirstName;
+  }
+
+  
+
+}
