c10a48d01f65f5456d3d973a546915807cea96c3
davmar3798 <davmar3798@students.epci.edu>
2013-02-07 18:47:19 -0800

    courses.java

---
+++ courses.java
@@ -0,0 +1,98 @@
+public class Courses {
+
+  private string courseID;
+
+  private string courseName;
+
+  private string preReqs;
+
+  private string termID;
+
+  private integer credits;
+
+  private float grade;
+
+  private string studentID;
+
+  private string teacherID;
+
+  private string getCourseID() 
+  {
+  return courseID;
+  }
+
+  public string setCourseID(string courseIdentity) 
+  {
+  this.courseID = courseIdentity;
+  }
+
+  public string getCourseName() 
+  {
+  return CourseName;
+  }
+
+  public string setCourseName(string Name) 
+  {
+  this.courseName = Name;
+  }
+
+  public string getPreReqs() 
+  {
+  return PreReqs;
+  }
+
+  public string setPreReqs(string PR) {
+  this.preReqs = PR;
+  }
+
+  public string getTermID() 
+  {
+  return termID;
+  }
+
+  public string setTermID(string TID) 
+  {
+  thid.termID = TID;
+  }
+
+  public integer getCredits() 
+  {
+  return credits;
+  }
+
+  public integer setCredits(int c) 
+  {
+  this.credits = c;
+  }
+
+  public float getGrade()
+  {
+  return grade;
+  }
+
+  public float setGrade(float g) 
+  {
+  this.grade = g;
+  }
+
+  public string getStudentID() 
+  {
+  return studentID;
+  }
+
+  public string setStudentID(string SID) 
+  {
+  this.studentID = SID;
+  }
+
+  public string getTeacherID() 
+  {
+  return teacherID;
+  }
+
+  public string setTeacherID(string TID)
+  {
+  this.teacherID = TID;
+  }
+
+}
