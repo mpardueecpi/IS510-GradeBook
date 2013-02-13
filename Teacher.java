81e7b0be9e0a86845045b2a1d851fc0337c840c4
mahaffeymg@me.com <mahaffey@server.example.private>
2013-02-12 21:21:12 -0500

    Teacher with assignment array

---
+++ Teacher.java
@@ -0,0 +1,13 @@
+//package edu.ecpi.is510.gradebook;
+import.java.util.scanner
+
+public class Teacher extends User{
+	private String assignmentArray = new String[] {"Homework 1", "Homework 2", "Test 1", "Test 2"};
+	private String teacherID = new String[] {"Sam1", "Steve3"};
+
+	public String getTeacherID(int teacherID){
+		return teacherID;
+		System.out.println(getTeacherID);
+	}
+
+}
